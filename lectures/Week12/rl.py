950cb63cdf93e5fcfd064737e145290325ded486
Bruce Xie <mebiusvista@gmail.com>
2018-04-09 22:57:41 -0400

    Update rl.py

--- lectures/Week12/rl.py
+++ lectures/Week12/rl.py
@@ -84,6 +84,11 @@ class Agent():
             qstr += str(self.Q[i]) + '\n'
         return qstr
 
+    def polish(self):
+        for i in range(len(self.Q)):
+            for j in range(len(self.Q[i])):
+                self.Q[i][j] = round(self.Q[i][j])
+
 if __name__ == "__main__":
     ''' set up the world'''
     w = World(R, 5)
@@ -128,6 +133,7 @@ if __name__ == "__main__":
 
         num_iter += 1
         if num_iter % report_steps == 0:
+            agent.polish()
             print(agent)
 
         if interactive:
